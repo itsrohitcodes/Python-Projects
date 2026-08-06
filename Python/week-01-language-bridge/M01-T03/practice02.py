d48ca11fa7f730a2dbc3034f85f64a17a6e8097e
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-06 14:52:52 +0530

    Pseudocode to Pyhton Code

---
+++ Python/week-01-language-bridge/M01-T03/practice02.py
@@ -0,0 +1,12 @@
+# Convert Pseudocode to Python
+
+# Pseudocode
+# Print "Welcome to Kodnest"
+# Print "Python Track Started"
+# Print "Let us begin coding"
+
+# Pyhton Code
+
+print ("Welcome to Kodnest")
+print ("Python Track Started")
+print ("Let us begin coding")
