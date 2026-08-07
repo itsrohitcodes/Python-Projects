40e6b362ea3ae78a2b8d9c3c23e8bc03c158eefe
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-07 15:13:49 +0530

    Data Types Values

---
+++ Python/week-02-Introduction-to-python/M02-T01/practice02.py
@@ -0,0 +1,19 @@
+# Convert Student Registration Values
+
+# Input the values
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+# Convert the values
+age = int(age_text)
+course_fee = float(course_fee_text)
+attempts_text = str(attempts)
+
+# Display the converted values and their data types
+print(age)
+print(type (age))
+print(course_fee)
+print(type (course_fee))
+print(attempts_text)
+print(type (attempts_text))
