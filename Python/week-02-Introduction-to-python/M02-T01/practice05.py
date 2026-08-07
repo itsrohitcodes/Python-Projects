9f1989a35d1ecd61d5aa9283961910c3ed5c4d1b
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-07 15:21:45 +0530

    Division and Floor Division

---
+++ Python/week-02-Introduction-to-python/M02-T01/practice05.py
@@ -0,0 +1,6 @@
+# Calculate the division and floor division of the two numbers
+first_number = int(input())
+second_number = int(input())
+
+print(first_number / second_number)
+print(first_number // second_number)
