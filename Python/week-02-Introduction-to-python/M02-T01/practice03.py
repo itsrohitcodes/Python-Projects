b00447abfc31783a2ccdcf00c71f00083e89ab35
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-07 15:18:24 +0530

    Age Calculation

---
+++ Python/week-02-Introduction-to-python/M02-T01/practice03.py
@@ -0,0 +1,9 @@
+# Fix the student age calculation
+
+# Take input values
+current_age = "21"
+current_age = int(current_age)
+next_age = current_age + 1
+
+# Print the calculated next age value
+print(f"Age next year: {next_age}")
