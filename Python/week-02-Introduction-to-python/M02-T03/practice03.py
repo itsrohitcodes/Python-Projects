0fe4432bbfdbead18df981188b5b0a1b62c68824
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-15 16:05:20 +0530

    Create and unpacked a tuple

---
+++ Python/week-02-Introduction-to-python/M02-T03/practice03.py
@@ -0,0 +1,17 @@
+# Create and unpacked a student record
+
+# Read and display student details
+name = input()
+course = input()
+score = int(input())
+
+# Create the tuple
+student_record = (name, course, score)
+
+# Unpack the tuple
+student_name, student_course, student_score = student_record
+
+# Display the unpacked values
+print(f"Name: {student_name}")
+print(f"Course: {student_course}")
+print(f"Score: {student_score}")
